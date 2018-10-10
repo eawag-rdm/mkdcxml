11adc5be5c91abeba7cef7f25b69cc79702b1154
Harald von Waldow <harald.vonwaldow@eawag.ch>
2018-10-10 12:20:53 +0200

    ufrume

--- mkdcxml/__init__.py
+++
@@ -1,1 +0,0 @@
-from .mkdcxml import MetaDataWriter
--- mkdcxml/__main__.py
+++
@@ -1,6 +0,0 @@
-from mkdcxml import MetaDataWriter
-from pkg_resources import resource_stream 
-
-dmw = MetaDataWriter(resource_stream(__name__,
-                                  'tests/metadata/snsf_dmp_guide.json'))
-dmw.writexml(resource_stream(__name__,'tests/output/snsf_dmp_guide.xml'))
